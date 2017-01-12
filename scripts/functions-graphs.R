8ec0c7ca590642059a72f6663c97ff1dac69a037
Andrey Koval <koval.andrey@gmail.com>
2017-01-12 14:56:04 -0500

    added trajectory plots

--- scripts/functions-graphs.R
+++ scripts/functions-graphs.R
@@ -1,3 +1,7 @@
+########################
+##### Plotting Coefficients   ###########
+#######################
+
 plot_coef <- function(
   x,
   label_,
@@ -58,7 +62,13 @@ plot_coef <- function(
 # 
 # levels(ds_catalog$label)
 
-matrix_coef <- function(x,term_group,model_type, process){
+# uses plot_coef() to create a matrix of plots
+matrix_coef <- function(
+  x,
+  term_group,
+  model_type,
+  process
+){
   # x <- ds_catalog
   # model_type="aefb"
   # process ="mmse"
@@ -86,8 +96,16 @@ matrix_coef <- function(x,term_group,model_type, process){
 }
 # matrix_coef(ds_catalog,"level","aefb","mmse")
 
-super_matrix <- function(x,model_type,process, folder_name,
-                         width, height,res){
+# used matrix_coef() to create a supermatrix of plots
+super_matrix <- function(
+  x,
+  model_type,
+  process, 
+  folder_name,
+  width, 
+  height,
+  res
+){
   # x <- ds_catalog
   # model_type = "aefb"
   # process = "mmse"
@@ -126,50 +144,307 @@ super_matrix <- function(x,model_type,process, folder_name,
 }
 
 
-##### old functions from terminal declide project ----------
+########################
+##### Prep Data for Anatomy   ###########
+#######################
 
-plot_trajectories <- function(d,sample_size=200){
-  set.seed(42)
-  ids <- sample(unique(d$id),sample_size)
-  g <-  d %>% 
-    ggplot2::ggplot(aes(x=DTIMEC,y=Y,color=class)) +
-    geom_line(aes(group=id), size=.8, alpha=.7)+
-    labs(y="MMSE",x="Time until death",color="Latent class")+
-    scale_colour_manual(values=colors_classes)+
-    main_theme#+
-  # theme(text = element_text(size=baseSize+6))
+# reaches into ls_catalog and extract modeled data 
+get_model_data <- function(
+  list_object,
+  model_name
+){
+  # model <- ls_catalog[["u1_12345_aefb_block"]]
+  # model <- ls_catalog[["u1_1234_aefb_block"]]
+  model <- list_object[[model_name]]
+  
+  # ds <- MplusAutomation::getSavedata_Data(model[["path"]])
+  
+  # function to elongate extracted data
+  ds_obs <- MplusAutomation::getSavedata_Data(model[["path"]])
+  # (names(ds_obs) <- gh5_variables)
+  head(ds_obs)
+  ds_obs$id <- 1:nrow(ds_obs) # create person id
+  ds_obs <- ds_obs[order(ds_obs$id), ] # sort for visual inspection
+  ds_obs[ds_obs$id==1, ] # structure of data for one individual
+  head(ds_obs)
+  
+  # define variable groups for elongation
+  possible_timepoints <- c(paste0("A_0",1:5),paste0("TIME_0",1:5))
+  (variables_longitudinal <- intersect(possible_timepoints,colnames(ds_obs)))
+  
+  # establish a wide format
+  ds_long <- ds_obs %>%
+    tidyr::gather_(key="variable", value="value", variables_longitudinal) %>%
+    dplyr::mutate(
+      varname = gsub("(\\w+)_(\\d+)","\\1", variable, perl=T),
+      wave    = gsub("(\\w+)_(\\d+)","\\2", variable, perl=T) %>% as.numeric()
+    ) %>% 
+    dplyr::select(-variable) %>% 
+    dplyr::arrange(id) %>% 
+    tidyr::spread(varname,value)  
+  # compute predicted scores
+  if( any(colnames(ds_long)=="QA")){ 
+    ds_long <- ds_long %>% 
+      dplyr::mutate(
+        A_HAT = IA + SA*TIME + QA*TIME
+      )
+  }else{
+    ds_long <- ds_long %>% 
+      dplyr::mutate(
+        A_HAT = IA + SA*TIME 
+      )
+  }
+  ds_long <- ds_long %>% 
+    tidyr::gather_(key="source", value = "value", c("A","A_HAT")) %>%
+    dplyr::mutate( 
+      source = car::Recode(source,"'A'='observed';'A_HAT'='predicted'")
+    ) 
+  # colnames(ds_long) <- tolower(colnames(ds_long))
+  return(ds_long)
 }
+# Usage:
+# ds_long <- ls_catalog %>% get_model_data("u1_12345_aefb_block")
 
-class_trajectories <- function(d,group,group_label,color_scale,sample_size=200){
-  # d <- ds_long_newc
-  # group <- "female" 
-  set.seed(42)
-  ids <- sample(unique(d$id),sample_size)
-  g <-  d %>% dplyr::filter(id %in% ids) %>% 
-    ggplot2::ggplot(aes_string(x="DTIMEC",y="Y",color=group)) +
-    geom_line(aes(group=id),size=.8,alpha=.7)+
-    facet_grid(.~class)+
-    scale_color_manual(values = color_scale)+
-    labs(y="MMSE",x="Time until death", color = group_label)+
+# takes the product of get_model_data() and covariate centers data and
+# 1) recenters predictors and 2) creates factors 
+prep_for_graph <- function(
+  ds_long,
+  centers
+){
+  # centers
+  # d <- ds_long
+  d <- ds_long %>%
+    # recover centers
+    dplyr::mutate(
+      age_bl = AGE_C80 + centers["age"],
+      edu = EDU_C7 + centers["edu"],
+      sbp = SBP_C167 + centers["sbp"]
+    ) %>% 
+    # create age_at_visit
+    dplyr::mutate(
+      age_at_visit   = age_bl + TIME,
+      years_since_bl = TIME
+    ) %>% 
+  # make factors
+  dplyr::mutate(
+    age_group_bl = Hmisc::cut2(age_bl,g=5 ),
+    age_group_bl = factor(age_group_bl, levels = rev(levels(age_group_bl))),
+    edu_group    = Hmisc::cut2(edu,g=5),
+    edu_group    = factor(edu_group, levels = rev(levels(edu_group))),
+    sbp_group    = Hmisc::cut2(sbp,g=5),
+    sbp_group    = factor(sbp_group, levels = rev(levels(sbp_group))),
+    female       = factor(FEMALE,c(0,1),c("male","female"))
+  ) 
+  # colnames(ds_long) <- tolower(colnames(ds_long))  
+  d <- d %>%
+    dplyr::select(-AGE_C80, -EDU_C7, -SBP_C167, -X_WEIGHT, -TIME, -FEMALE)
+  # d <- d %>% 
+  #   tidyr::gather_(key="metric",value="time", c("years_since_bl","age_at_visit")) %>% 
+  #   dplyr::mutate(
+  #     source_metric = paste0(source,"_",metric),
+  #     source_metric = factor(
+  #       source_metric,
+  #       levels = c(
+  #         "observed_years_since_bl",
+  #         "predicted_years_since_bl",
+  #         "observed_age_at_visit" ,
+  #         "predicted_age_at_visit"
+  #       ),
+  #       labels = c(
+  #         "Observed over TIME",
+  #         "Observed over AGE",
+  #         "Modeled over TIME",
+  #         "Modeled over AGE"
+  #       ))
+  #   )
+  return(d)
+}
+# Usage: 
+# ds_long <- ds_long %>% prep_for_graph(centers)
+
+
+########################
+##### Trajectories   ###########
+#######################
+# takes a data set for a single model, output of prep_for_graph()
+plot_trajectories <- function(
+  d,
+  time_var,
+  sample_size=100
+){
+  # d <- ds_long
+  # time_var = "age_at_visit"
+  # sample_size = 100
+  
+  if(!sample_size=="max"){
+    set.seed(42)
+    ids <- sample(unique(d$id),sample_size)
+    dd <- d %>% dplyr::filter(id %in% ids)
+  }else{dd <- d}
+  # dd <- d
+  g <-  dd %>%  
+    # ggplot2::ggplot(aes_string(x="age_at_visit",y="value",color="female")) +
+    ggplot2::ggplot(aes_string(x=time_var,y="value")) +
+    geom_line(aes(group=id),size=.9,alpha=.075)+
+    geom_point(size=.8, alpha=.15)+
+    facet_grid(.~source)+
+    scale_y_continuous(limits = c(-10,40), breaks=seq(-10,40,10))+
+    # scale_color_manual(values = color_scale)+
+    # labs(y="MMSE",x="Time until death", color = group_label)+
     main_theme+
     theme(text = element_text(size=baseSize+4))
+  # print(length(unique(dd$id)))
   g
 }
+# Usage:
+# plot_trajectories(ds_long, "age_at_visit","max")
+# plot_trajectories(ds_long, "age_at_visit",100)
 
-class_traj_matrix <- function(newc,octo,group,group_label,color_scale){
+# used output of plot_trajectories()
+trajectory_matrix <- function(
+  ls_catalog,
+  model_name,
+  sample_size_
+){
+  # model_name <- "u1_12345_aefb_mmse"
+  # sample_size_ = 100
+  # sample_size_ = "max"
+  
+  
+  d <- ls_catalog %>% 
+    get_model_data(model_name) %>% 
+    prep_for_graph(centers) 
+  
+  model_number <- ls_catalog[[model_name]]$model_number
+  wave_set <- ls_catalog[[model_name]]$wave_set
+  process <- toupper(ls_catalog[[model_name]]$process)
+  
+  model_shape <- ifelse(model_number=="u1","Linear",
+                        ifelse(model_number=="u2","Quadratic",NA))
+  model_shape <- ifelse(model_number=="u1","L",
+                        ifelse(model_number=="u2","Q",NA))
+  model_id <- paste0(model_shape,"-",wave_set)
+  
+  g1 <- d %>% plot_trajectories("years_since_bl",sample_size_)
+  g1 <- g1 + coord_cartesian(xlim=c(0,8))+
+    scale_x_continuous(breaks=seq(0,8,2))
+  
+  g2 <- d %>% plot_trajectories("age_at_visit",sample_size_)
+  g2 <- g2 + coord_cartesian(xlim=c(80,100),expand = TRUE)+
+    scale_x_continuous(breaks=seq(80,100,5))
+  
+  
   pm <- GGally::ggmatrix(
     list(
-      "newc" = class_trajectories(newc,group,group_label,color_scale),
-      "octo" = class_trajectories(octo,group,group_label,color_scale)
+      "time" = g1,
+      "age" =  g2
     ),
     nrow = 1, ncol = 2,
-    title = "MMSE trajectories plotted as a function of years to death\n(intercept placed at two years before death)", 
-    xAxisLabels = c("Newcastle 85+","OCTO-Twin"), 
-    yAxisLabels = "MMSE score",
-    legend = 2
+    # title = paste0(process," trajectories as a function of time. Shape: ",model_shape,", Waveset: ", wave_set), 
+    title = paste0(process," trajectories as a function of time. Model: ",model_shape,"-", wave_set), 
+    xAxisLabels = c("Years since baseline","Age at visit"), 
+    yAxisLabels = "MMSE score"#,
+    # legend = 2
   ) + theme(
-    legend.position = "bottom"
-    
+    plot.title = element_text(size=baseSize+7),
+    strip.text = element_text(size=baseSize+5)
+    # strip.text.y = element_text(size=baseSize+7)
   )
   pm
 }
+# Usage:
+# ls_catalog %>% trajectory_matrix("u1_12345_aefb_mmse", 100)
+
+
+
+
+
+trajectory_supermatrix <- function(
+  ls_catalog
+  # folder_name,
+  # width, 
+  # height,
+  # res
+){
+  sample_size = 100
+  # x <- ds_long
+  # model_type = "aefb"
+  # process = "mmse"
+  folder_name = "./reports/model-grapher/graphs-1/"
+  height = 1200
+  width = 1400
+  res = 600
+  
+  g1 <- ls_catalog %>% 
+    get_model_data("u1_12345_aefb_mmse") %>% 
+    prep_for_graph(centers) %>%  
+    trajectory_matrix(sample_size)
+  
+  g2 <- ls_catalog %>% 
+    get_model_data("u2_12345_aefb_mmse") %>% 
+    prep_for_graph(centers) %>%  
+    trajectory_matrix(sample_size)
+  
+  g3 <-ls_catalog %>% 
+    get_model_data("u1_1234_aefb_mmse") %>% 
+    prep_for_graph(centers) %>%  
+    trajectory_matrix(sample_size)
+  
+  g4 <-ls_catalog %>% 
+    get_model_data("u2_1234_aefb_mmse") %>% 
+    prep_for_graph(centers) %>%  
+    trajectory_matrix(sample_size)
+  
+  g5 <-ls_catalog %>% 
+    get_model_data("u1_123_aefb_mmse") %>% 
+    prep_for_graph(centers) %>%  
+    trajectory_matrix(sample_size)
+  
+  g6 <-ls_catalog %>% 
+    get_model_data("u2_123_aefb_mmse") %>% 
+    prep_for_graph(centers) %>%  
+    trajectory_matrix(sample_size)
+  
+  g7 <-ls_catalog %>% 
+    get_model_data("u1_135_aefb_mmse") %>% 
+    prep_for_graph(centers) %>%  
+    trajectory_matrix(sample_size)
+  
+  g8 <-ls_catalog %>% 
+    get_model_data("u2_135_aefb_mmse") %>% 
+    prep_for_graph(centers) %>%  
+    trajectory_matrix(sample_size)
+  
+  path_save = paste0(folder_name,"mmse",".png")
+  png(filename = path_save, width = width, height = height,res = res)
+  
+  vpLayout <- function(rowIndex, columnIndex) { return( viewport(layout.pos.row=rowIndex, layout.pos.col=columnIndex) ) }
+  grid::grid.newpage()
+  #Defnie the relative proportions among the panels in the mosaic.
+  layout <- grid::grid.layout(nrow=8, ncol=1,
+                              widths=grid::unit(c(1) ,rep("null",1)),
+                              heights=grid::unit(rep(.125,8), rep("null",8))
+  )
+  grid::pushViewport(grid::viewport(layout=layout))
+  # main_title <- toupper(dsL$study_name[1])
+  # main_title <- paste0(toupper(mp$study_name), " \n ", mp$subgroup, " \n ",
+  #                      "N = ", sample_N)
+  # main_title <- paste0(toupper(process)," : model parameters")
+  # grid::grid.text(main_title, vp = grid::viewport(layout.pos.row = 1, layout.pos.col = 1:3))
+  print(g1, vp=grid::viewport(layout.pos.row=1,layout.pos.col=1))
+  print(g2, vp=grid::viewport(layout.pos.row=2,layout.pos.col=1))
+  print(g3, vp=grid::viewport(layout.pos.row=3,layout.pos.col=1))
+  print(g4, vp=grid::viewport(layout.pos.row=4,layout.pos.col=1))
+  print(g5, vp=grid::viewport(layout.pos.row=5,layout.pos.col=1))
+  print(g6, vp=grid::viewport(layout.pos.row=6,layout.pos.col=1))
+  print(g7, vp=grid::viewport(layout.pos.row=7,layout.pos.col=1))
+  print(g8, vp=grid::viewport(layout.pos.row=8,layout.pos.col=1))
+  
+  
+  dev.off()
+  # return(grid::popViewport(0))
+}
+
+ls_catalog %>% trajectory_supermatrix()
+
+
